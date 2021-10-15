469f5bd67826761a1d5dfe77383882a845248966
Estelle Révillion <estelle.revilliion@live.fr>
2021-10-15 11:00:50 +0200

    MappingUtilisateur

--- src/main/java/model/utilisateur/Locataire.java
+++ src/main/java/model/utilisateur/Locataire.java
@@ -1,28 +1,34 @@
 package model.utilisateur;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import model.logement.Chambre;
+
+@Entity
+@DiscriminatorValue("locataire")
 public class Locataire extends Utilisateur {
 	private boolean recherche;
 	private String description;
 	private Situation situation;
 	private Dossier dossier;
 	
-	
-	public Locataire() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+	@ManyToOne
+	private Chambre chambre;
 	
 	
+	public Locataire() {}
 
 	public Locataire(String nom, String prenom, Civilite civ, String email, String tel, boolean recherche,
-			String description, Situation situation, Dossier dossier) {
+			String description, Situation situation, Dossier dossier, Chambre chambre) {
 		super(nom, prenom, civ, email, tel);
 		this.recherche = recherche;
 		this.description = description;
 		this.situation = situation;
 		this.dossier = dossier;
+		this.chambre = chambre;
 	}
-
 
 
 	public boolean isRecherche() {
@@ -41,12 +47,37 @@ public class Locataire extends Utilisateur {
 		this.description = description;
 	}
 
-	@Override
-	public String toString() {
-		return "Locataire [recherche=" + recherche + ", description=" + description + ", nom=" + nom + ", prenom="
-				+ prenom + ", civ=" + civ + ", email=" + email + ", tel=" + tel + "]";
+	public Situation getSituation() {
+		return situation;
 	}
 
+	public void setSituation(Situation situation) {
+		this.situation = situation;
+	}
+
+	public Dossier getDossier() {
+		return dossier;
+	}
+
+	public void setDossier(Dossier dossier) {
+		this.dossier = dossier;
+	}
+
+	public Chambre getChambre() {
+		return chambre;
+	}
+
+	public void setChambre(Chambre chambre) {
+		this.chambre = chambre;
+	}
+
+	@Override
+	public String toString() {
+		return "Locataire [recherche=" + recherche + ", description=" + description + ", situation=" + situation
+				+ ", dossier=" + dossier + ", chambre=" + chambre + "]";
+	}
+
+	
 	
 	
 
--- src/main/java/model/utilisateur/Proprio.java
+++ src/main/java/model/utilisateur/Proprio.java
@@ -1,17 +1,44 @@
 package model.utilisateur;
 
+import java.util.List;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import model.logement.Logement;
+
+@Entity
+@DiscriminatorValue("proprio")
 public class Proprio extends Utilisateur {
 
+	@OneToMany(mappedBy = "proprio")
+	private List<Logement> biens;
 	
-	public Proprio() {
-		super();
-		// TODO Auto-generated constructor stub
+	public Proprio() {}
+
+	
+	public Proprio(String nom, String prenom, Civilite civ, String email, String tel, List<Logement> biens) {
+		super(nom, prenom, civ, email, tel);
+		this.biens = biens;
 	}
 
-	public Proprio(String nom, String prenom, Civilite civ, String email, String tel) {
-		super(nom, prenom, civ, email, tel);
-		// TODO Auto-generated constructor stub
+
+	public List<Logement> getBiens() {
+		return biens;
 	}
+
+	public void setBiens(List<Logement> biens) {
+		this.biens = biens;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Proprio [biens=" + biens + "]";
+	}
+	
+	
 	
 	
 
